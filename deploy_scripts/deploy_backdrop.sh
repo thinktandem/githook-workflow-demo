f52cf1906a116973af81f5938a96f489bbc80b88
Geoff St. Pierre <serundeputy@gmail.com>
2017-03-04 16:52:43 -0500

    Updating deploy script to reset non-bare git repo.

--- deploy_scripts/deploy_backdrop.sh
+++ deploy_scripts/deploy_backdrop.sh
@@ -4,6 +4,12 @@
 # Post code deploy actions
 ##
 
+# move to the git repo.
+cd /var/www/backdrop
+
+# Reset the repo since we are pusing to a non --bare repo.
+git reset --hard
+
 # Clear cache.
 /usr/local/bin/drush cc all
 
